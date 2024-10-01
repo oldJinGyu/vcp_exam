ad7c9b372e6739960aad134af1087515a3b09a32
OldJinGyu <chwlsrb00225@naver.com>
2024-10-01 18:03:54 +0900

    init

---
+++ app/src/main/java/com/example/vcp_exam/ActivityMain.kt
@@ -0,0 +1,20 @@
+package com.example.vcp_exam
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.vcp_exam.databinding.ActivityMainBinding
+
+class ActivityMain : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+    }
+}
